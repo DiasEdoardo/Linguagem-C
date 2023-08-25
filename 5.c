b15765101295805adf46061452bfd817ae4afc46
DiasEdoardo <eduardodiasmachado7979@gmail.com>
2023-08-25 15:08:15 -0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
     int main(){
-        int meses,anos,dias,idias,imeses,im2,diastotal;
+        int meses,anos,dias,idias,imeses,diastotal;
 
 
         printf("Qual a sua idade em anos?");
@@ -11,19 +11,11 @@
         printf("E quantos dias?");
         scanf("%i",&dias);
 
-        imeses=anos*365; // calcular anos transformando em meses
-        idias=imeses/30; //calcular meses em dias
-       
-
-        im2=meses/30; // calcular meses em dias
-        diastotal=im2+idias+dias;
+        imeses=anos*365; // calcular anos em dias
+        idias=meses*30; // calcular meses em dias
+        diastotal= idias+imeses+dias;
 
         printf("O total de dias eh %i",diastotal);
-        
-        
-
-
-
         return 0;
 
     }
