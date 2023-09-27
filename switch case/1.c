068e1eeee6610c79ad80b8189acbfb379eae434f
dias <114160248+DiasEdoardo@users.noreply.github.com>
2023-09-27 11:08:04 -0300

    C

--- switch case/1.c
+++ switch case/1.c
@@ -1,7 +1,34 @@
 #include<stdio.h>
 
     int main(){
-        printf
+        int x,y,z,opcao;
+        printf("Escolha dois numeros para fazer uma operação:\n");
+        scanf("%i""%i",&x,&y);
+        printf("selecione uma opção de operação:\n1-Soma\n2-Divisão\n3-Subtração\n4-Multiplicação\n");
+        scanf("%i",&opcao);
+        switch (opcao)
+        {
+        case 1:
+            z=x+y;
+            printf("a soma dos numeros é: %i",z);
+            break;
+        case 2:
+            z=x/y;
+            printf("a divisão dos numeros é: %i",z);
+            break;
+        case 3:
+            z=x-y;
+            printf("a subtração dos numeros é: %i",z);
+            break;
+        case 4:
+            z=x*y;
+            printf("a multiplicação dos numeros é: %i",z);
+            break;
+        
+        default:
+            printf("a opção é invalida, por favor tente novamente");
+            break;
+        }
 
         return 0;
     }
